9f157cea9a6d09e264e3a12782716ce848a201b9
Carlos Martins <carlosemartins97@gmail.com>
2021-05-26 09:19:42 -0300

    finalizado bloco acesso rapido do footer

--- wp-content/themes/modulo-3/assets/views/includes/footer.php
+++ wp-content/themes/modulo-3/assets/views/includes/footer.php
@@ -1,4 +1,64 @@
 <?php include_once get_template_directory() . '/assets/views/includes/curriculo.php' ?>
+<section class="contato-container">
+    <div class="contato-content">
+        <div class="contato-acesso-rapido">
+            <h3>Acesso Rápido</h3>
+            <ul>
+                <li>sobre nós</li>
+                <li>para sua empresa</li>
+                <li>para profissionais</li>
+                <li>vagas</li>
+                <li>saiu na mídia</li>
+                <li>quero saber mais</li>
+                <li>escola do comportamento</li>
+                <li>blog</li>
+            </ul>
+        </div>
+        <div class="contato-onde-estamos">
+            <div class="onde-estamos-bloco">
+                <h3>onde estamos</h3>
+                <span>Av. Senador Feijí 686</span>
+                <span>Vila Mathias, Santos - SP</span>
+                <span>CEP/11015-504</span>
+            </div>
+            <div class="onde-estamos-bloco">
+                <h3 id="contato-acompanhe-nos">acompanhe-nos</h3>
+                <div class="onde-estamos-social">
+                    <a href="#"><img src="<?=get_template_directory_uri()?>/dist/img/footer/face-icon.png" alt="Ícone do facebook"></a>
+                    <a href="#"><img src="<?=get_template_directory_uri()?>/dist/img/footer/insta-icon.png" alt="Ícone do Instagram"></a>
+                    <a href="#"><img src="<?=get_template_directory_uri()?>/dist/img/footer/store-icon.png" alt="Ícone do Youtube"></a>
+                </div>
+            </div>
+        </div>
+        <div class="contato-contatos">
+            <div class="contato-contatos-content">
+                <h3>contatos</h3>
+                <a href="tel:13991590164" class="contato-contatos-card">
+                    <img src="<?=get_template_directory_uri()?>/dist/img/footer/phone-icon.png" alt="Ícone de telefone">
+                    <div class="contatos-card-info">
+                        <small>Comercial</small>
+                        <span>(13)<strong> 99159.0164</strong></span>
+                    </div>
+                </a>
+                <a href="tel:13991037559" class="contato-contatos-card">
+                    <img src="<?=get_template_directory_uri()?>/dist/img/footer/phone-icon.png" alt="Ícone de telefone">
+                    <div class="contatos-card-info">
+                        <small>suporte ao candidato</small>
+                        <span>(13)<strong>99103-7559</strong></span>
+                    </div>
+                </a>
+                <a href="tel:13991037559" class="contato-contatos-card">
+                    <img src="<?=get_template_directory_uri()?>/dist/img/footer/phone-icon.png" alt="Ícone de telefone">
+                    <div class="contatos-card-info">
+                        <small>sucesso do cliente</small>
+                        <span>(13)<strong>99103-7559</strong></span>
+                    </div>
+                </a>
+            </div>
+            <a href="mailto:contato@sartoridho.com.br?subject=Dúvida">contato@sartoridho.com.br</a>
+        </div>
+    </div>
+</section>
 <footer>
     <div class="footer-container">
         <a class="footer-link-logo" href="#"><img class="footer-logo" src="<?php echo get_template_directory_uri()?>/dist/img/logo.png" alt="Logo do Grupo Sartori"></a>
