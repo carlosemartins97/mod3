b68c1c93941253c3d56e555ce61d1eb67be25406
Carlos Martins <carlosemartins97@gmail.com>
2021-06-02 16:36:33 -0300

    Finalizado responsividade pagina Sobre

--- wp-content/themes/modulo-3/template-sobre.php
+++ wp-content/themes/modulo-3/template-sobre.php
@@ -108,6 +108,7 @@
 
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://kit.fontawesome.com/5ced3d7c26.js" crossorigin="anonymous"></script>
     <script src="<?= get_template_directory_uri()?>/assets/js/main.js"></script>
 </body>
 </html>
